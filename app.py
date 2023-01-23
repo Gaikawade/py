037685c7ddda20202dc569e19705afdd3064d8c8
Gaikawade <gmhs6199@gmail.com>
2023-01-23 23:58:23 +0530

    registration api

--- app.py
+++ app.py
@@ -1,44 +1,40 @@
-# importing Flask library
-from flask import Flask
-# importing Connect and Error class from mysql-connector-python library 
-from mysql.connector import connect, Error
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
-# Creating and instance for Flask
 app = Flask(__name__)
 
-# function to connect MySQL database
-def connect_to_database():
-    print('before connection to database')
-    try:
-        # Stroing the connection into a separate variable
-        # connect() function is used to establish a connection to a MySQL server using the following parameters:
-        connection = connect(
-            host='localhost',
-            port='3306',
-            user='root',
-            password='mahesh123',
-            database='flask'
-        )
-        print('connection established')
-        return connection
-    except Error as e:
-        print(e)
-        return e
+app.secret_key = 'xyzsdfg'
 
-# Route Decorator
-@app.route('/')
-# Main API function
-def index():
-    cnx = connect_to_database()
-    # a cursor object is used to execute SQL commands on a MySQL database
-    cursor = cnx.cursor()
-    # cursor.execute() method is used to execute a SQL SELECT statement on the table 'employee'
-    cursor.execute('select * from employee')
-    # fetchall() is a method of a cursor object in python that is used to retrieve all the rows from the result set of a SQL query. It returns a list of tuples, where each tuple represents a row 
-    result = cursor.fetchall()
-    # retult = [ (row-1), (row-2), (row-3) ]
-    # returning the result in a string format
-    return str(result)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'mahesh123'
+app.config['MYSQL_DB'] = 'flask'
+
+mysql = MySQL(app)
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    if request.method == 'POST' and 'name' in request.form and 'email' in request.form and 'password' in request.form :
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+        if account:
+            return 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            return 'Invalid email address !'
+        elif not name or not password or not email:
+            return 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO user VALUES ( % s, % s, % s)', (name, email, password, ))
+            mysql.connection.commit()
+            return 'You have successfully registered !'
+    elif request.method == 'POST':
+        return 'Please fill out the form !'
 
 if __name__ == '__main__':
     app.run(debug=True)
